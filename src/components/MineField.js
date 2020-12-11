c8a20a21a0911383b74729653985a1664e328cc5
lucas-serafim <lucas.serafim159@gmail.com>
2020-12-10 21:26:33 -0300

    New functions in the Logic.js class

--- src/components/MineField.js
+++ src/components/MineField.js
@@ -5,7 +5,7 @@ import Field from './Field'
 export default props => {
    const rows = props.board.map((row, r) => {
       const columns = row.map((field, c) => {
-         return <Field {...field} key = {c} opened />
+         return <Field {...field} key = {c} />
       })
       return <View key = {r} style = {{ flexDirection: 'row' }}>{columns}</View>
    })
